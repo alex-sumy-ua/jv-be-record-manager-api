07c19a774019e262fed89cc9b9192705b6d8b581
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-09-05 17:16:30 +0100

    feat: Task 3: Application Properties for Development - completed

---
+++ src/main/java/com/northcoders/jv_be_record_manager_api/JvBeRecordManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_be_record_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBeRecordManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvBeRecordManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/jv_be_record_manager_api/JvBeRecordManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_be_record_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvBeRecordManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
